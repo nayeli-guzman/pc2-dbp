fc82ddc4bc92e9d78e5d45a26e16bfe822527371
Dylan Cabezas <dylan.cabezas@utec.edu.pe>
2024-11-19 15:12:24 -0500

    Updates design

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,18 @@
-export default function MainPage(){
-    return(
-    <div className="space-y-10">
-    <button className="bg-red-500">
-        Añadir
-    </button>
-    <button className="bg-red-500">
-        Ver
-    </button>
-    <button className="bg-red-500">
-        Editar
-    </button>
-    <button className="bg-red-500">
-        Borrar
-    </button>
-    </div>)
+export default function MainPage() {
+    return (
+        <div className="space-y-4 p-4">
+            <button className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">
+                Añadir
+            </button>
+            <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+                Ver
+            </button>
+            <button className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
+                Editar
+            </button>
+            <button className="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600">
+                Borrar
+            </button>
+        </div>
+    );
 }

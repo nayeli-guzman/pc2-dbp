883fc9bf1e3a2047d9f2428c8663268019ebd8b4
Nayeli <nayeli.guzman@utec.edu.pe>
2024-11-18 20:04:25 -0500

    base

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import LoginPage from "./pages/LoginPage"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-500 text-white p-4">
+        testing
+      </div>
+    </>
+  )
+}
+
+export default App

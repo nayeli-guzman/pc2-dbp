a9080993a28a22614700e7c6c39742270a6ec5cd
Dylan Cabezas <dylan.cabezas@utec.edu.pe>
2024-11-19 15:01:48 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import LoginPage from './pages/LoginPage';
-
-
-interface Course {
-  id: number;
-  name: string;
-}
-
-const CoursesPage: React.FC = () => {
-  const [courses, setCourses] = useState<Course[]>([]);
-
-  useEffect(() => {
-    const fetchedCourses: Course[] = [
-      { id: 1, name: 'Course 1' },
-      { id: 2, name: 'Course 2' },
-      { id: 3, name: 'Course 3' },
-    ];
-    setCourses(fetchedCourses);
-  }, []);
-
-  return (
-    <div>
-      <h1>Courses</h1>
-      <ul>
-        {courses.map(course => (
-          <li key={course.id}>{course.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export { CoursesPage };
+import React from 'react';
+import CoursesPage from './pages/Cursos';
 
 function App() {
-
   return (
-    <>
-      <div className="bg-red-500 text-white p-4">
-        testing
-      </div>
-    </>
-  )
+    <div className="bg-gray-100 min-h-screen p-4">
+      <CoursesPage />
+    </div>
+  );
 }
 
-export default App
+export default App;
